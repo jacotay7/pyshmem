743d5902bc1809c8e0028e3f83f70cca6581321c
jacotay7 <jtaylor@keck.hawaii.edu>
2026-04-30 15:01:57 -1000

    version 1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, SRC)
 project = "pyshmem"
 author = "Jacob Taylor"
 copyright = "2026, Jacob Taylor"
-release = "1.0.0"
+release = "1.0.1"
 
 extensions = [
     "myst_parser",
--- src/pyshmem/__init__.py
+++ src/pyshmem/__init__.py
@@ -2,7 +2,7 @@
 
 from pyshmem._shared import SharedMemory, create, gpu_available, open, unlink
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = [
     "SharedMemory",
